5f6cba1831bef1f1fa1ae948a0b4e5a8a09abd43
tipubandlapalli <bstippusultan@gmail.com>
2025-05-06 17:57:27 +0530

    api key hide method

--- src/main/java/org/example/externalapi/WeatherService.java
+++ src/main/java/org/example/externalapi/WeatherService.java
@@ -1,6 +1,7 @@
 package org.example.externalapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -8,14 +9,14 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class WeatherService {
-    public static final String apiKey = "apiKey";
+    @Value("${apiKey}")
+    public  String apiKey;
     public static final String reqString = "https://api.weatherapi.com/v1/current.json?key=API&q=CITY&aqi=yes";
     @Autowired
     private RestTemplate restTemplate;
 
     public ResponseEntity<?> getWeather(String city) {
         String finalRequestString = reqString.replace("CITY", city).replace("API", apiKey);
-
         return  restTemplate.exchange(finalRequestString, HttpMethod.GET, null, String.class);
     }
 }
